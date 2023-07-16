d70c6dea51ecd0ea3b7e06eedcfb4c3fb8724ccb
Aleksandr <titamoto@gmail.com>
2023-07-16 11:16:35 -0700

    omplete lab

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,30 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
-    
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
         
+    
+    def get_title(self):
+        return self._title
+    
+    def get_page_count(self):
+        return self._page_count
+    
+    def set_title(self, title):
+        if type(title) == str:
+            self._title = title
+    
+    def set_page_count(self, page_count):
+        if type(page_count) == int:
+            self._page_count = page_count
+        else:
+            print("page_count must be an integer")
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+    
+
+    title = property(get_title, set_title)
+    page_count = property(get_page_count, set_page_count)
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    
+    def __init__(self, brand, size, condition=None):
+        self.brand = brand
+        self.size = size
+        self.condition = condition
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
+
+
+    def get_size(self):
+        return self._size
+    
+    def set_size(self, size):
+        if type(size) == int:
+            self._size = size
+        else:
+            print("size must be an integer")
+
+    size = property(get_size, set_size)
